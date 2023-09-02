85b11b914f71bc30a3c547d64f8ae638f1828f17
ahmed <ahmedmaher7200@gmail.com>
2023-09-01 19:06:41 -0700

    t

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,27 +1,75 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
- * main - prints sum of 2 numbers
- * @argc: number of arguments
- * @argv: pointer to array of arguments
+ * _atoi - converts a string to an integer
+ * @s: string
  *
- * Return: 0 (Success),no-zero-fail
+ * Return: the int converted from the string
+ */
+
+int _atoi(char *s)
+{
+	int l, d, m, len, h, digit;
+
+	l = 0;
+	d = 0;
+	m = 0;
+	len = 0;
+	h = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (l < len && h == 0)
+	{
+		if (s[l] == '-')
+			++d;
+
+		if (s[l] >= '0' && s[l] <= '9')
+		{
+			digit = s[l] - '0';
+			if (d % 2)
+				digit = -digit;
+			m = m * 10 + digit;
+			h = 1;
+			if (s[l + 1] < '0' || s[l + 1] > '9')
+				break;
+			h = 0;
+		}
+		l++;
+	}
+
+	if (h == 0)
+		return (0);
+
+	return (m);
+}
+
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: pointer toarray of arguments
+ *
+ * Return: 0 (Success), 1 (Error)
 */
 
 int main(int argc, char *argv[])
 {
-	int sum;
+	int result, n1, n2;
 
-	if (argc == 3)
-	{
-		sum = atoi(argv[1]) * atoi(argv[2]);
-		printf("%d\n", sum);
-	}
-	else
+	if (argc < 3 || argc > 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+
+	n1 = _atoi(argv[1]);
+	n2 = _atoi(argv[2]);
+	result = n1 * n2;
+
+	printf("%d\n", result);
 
 	return (0);
 }
