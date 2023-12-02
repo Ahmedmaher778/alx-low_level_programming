f9cf83b692941bf3a0096aa851c3561da20f2c20
Ahmedmaher778 <ahmedmaher7200@gmail.com>
2023-12-02 13:47:30 -0800

    c

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,53 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - Add an item to Hash table.
+ * @ht: A pointer to the hash table.
+ * @key: key of the added item to the hash table.
+ * @value: pair value of the key.
+ * Return: Upon failure - 0.
+ *         else - 1.
+*/
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new;
+	char *value_copy;
+	unsigned long int inde, l;
+
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+
+	value_copy = strdup(value);
+	if (value_copy == NULL)
+		return (0);
+
+	inde = key_inde((const unsigned char *)key, ht->size);
+	for (l = inde; ht->array[l]; l++)
+	{
+		if (strcmp(ht->array[l]->key, key) == 0)
+		{
+			free(ht->array[l]->value);
+			ht->array[l]->value = value_copy;
+			return (1);
+		}
+	}
+
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+	{
+		free(value_copy);
+		return (0);
+	}
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (0);
+	}
+	new->value = value_copy;
+	new->next = ht->array[inde];
+	ht->array[inde] = new;
+
+	return (1);
+}
